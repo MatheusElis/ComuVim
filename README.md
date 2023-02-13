f8cc63c62e01de1a5d424a7cb34ac73ce8c5de56
Matheus Elis <matheus.elis@2rpnet.com>
2023-02-13 14:19:45 -0300

    feat: adding readme and autoinstall

---
+++ README.md
@@ -0,0 +1,14 @@
+## Installation
+
+Make a backup of your current nvim and shared folder
+
+```
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+```
+
+Clone the repository
+```
+git clone https://github.com/MatheusElis/nvim-profile ~/.config/nvim
+nvim +PackerSync
+```
--- lua/elis/packer.lua
+++ lua/elis/packer.lua
@@ -1,6 +1,15 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    is_bootstrap = true
+    vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+    vim.cmd [[packadd packer.nvim]]
+end
+
+
 return require('packer').startup(function(use)
         -- Packer can manage itself
         use 'wbthomason/packer.nvim'
@@ -75,4 +84,17 @@ return require('packer').startup(function(use)
         use {
             "folke/which-key.nvim",
         }
+        if is_bootstrap then
+            require('packer').sync()
+        end
     end)
+
+-- You'll need to restart nvim, and then it will work.
+if is_bootstrap then
+  print '=================================='
+  print '    Plugins are being installed'
+  print '    Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
+end
